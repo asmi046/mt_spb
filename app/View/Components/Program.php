0df6e9af6b8920b175f80b3901eb1ab0fcb0b053
asmi046 <asmi046@gmail.com>
2024-09-17 15:33:45 +0300

    Доверстан блок контактов

--- app/View/Components/Program.php
+++ app/View/Components/Program.php
@@ -8,12 +8,9 @@ use Illuminate\View\Component;
 
 class Program extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
-        //
+
     }
 
     /**
